c1035dac6cf6f9105e8f6385808f0990755fa8fe
Adam Spannbauer <SpannbauerAdam@gmail.com>
2019-08-16 16:59:53 -0400

    rename main script

--- stitch.py
+++ stitch.py
@@ -9,9 +9,7 @@ from im_join_reduce import im_join_reduce
 import utils
 
 ap = argparse.ArgumentParser()
-ap.add_argument('-i', '--input',
-                # default='images/headshot.jpeg',
-                default='image_pieces',
+ap.add_argument('-i', '--input', default='image_pieces',
                 help='Path to image/dir build with')
 ap.add_argument('-m', '--min_kp', default=40, type=int,
                 help="Minimum number of keypoint matches to perform join.")
